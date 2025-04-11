30c9708ce4976fc79649ae5cf89beb5852b017ef
DVSY7 <gbytkdduf1004@naver.com>
2025-04-11 23:23:06 +0900

    feat:회원정보 입력란 추가가

--- client/src/componant/login.js
+++ client/src/componant/login.js
@@ -7,7 +7,7 @@ export default function Login() {
   // 글자 투명도
   const text_opacity = 'text-white text-opacity-70';
   // 인풋요소 클레스
-  const input_element = `pl-2 h-8 text-black rounded-sm text-[1rem] w-60 placeholder:${text_size}`;
+  const input_element = `pl-2 h-8 text-black rounded-sm text-[1rem] w-60 placeholder:text-[0.8rem]`;
   // 버튼요소 클레스
   const button_element =
     'w-48 h-8 rounded-md sm:w-[15rem] font-bold';
@@ -17,8 +17,8 @@ export default function Login() {
   const hover = 'hover:text-opacity-100 hover:text-[0.85rem]';
   // form 레이아웃 설정
   const setForm = `flex flex-col gap-4 justify-center items-center relative h-screen sm:w-[450px] w-screen sm:float-end text-white`;
-
-  
+  // sexButton 설정정
+  const [sexButton, setSexButton] = useState("");
   // 회원가입 클릭 이벤트
   const [signinHidden, setSigninHidden] = useState("flex");
   const [signupHidden, setSignupHidden] = useState("hidden");
@@ -30,6 +30,13 @@ export default function Login() {
   const addSignin = ()=>{
     setSignupHidden("hidden");
     setSigninHidden("flex");
+  }
+  // sexButton 액션
+  const handleChangeSexButton = (e,sex)=>{
+    e.preventDefault();
+    if(sexButton !== sex){
+      setSexButton(sex);
+    }
   }
 
   return (
@@ -80,7 +87,7 @@ export default function Login() {
               Kakao
             </button>
             {/* 회원가입 */}
-            <div onClick={addSignup} className={`h-[20px] cursor-pointer ${text_size} ${text_opacity} ${hover} mt-32`}>계정이 없으신가요 ? <span className={"text-white text-opacity-100"}>가입하기</span></div>
+            <div onClick={addSignup} className={`h-[20px] cursor-pointer ${text_size} ${text_opacity} ${hover} mt-20`}>계정이 없으신가요 ? <span className={"text-white text-opacity-100"}>가입하기</span></div>
             {/* 비밀번호 찾기 */}
             <div className={`h-[20px] cursor-pointer ${text_size} ${text_opacity} ${hover}`}>비밀번호를 잊으셨나요 ?</div>
           </form>
@@ -96,6 +103,7 @@ export default function Login() {
             {/* 아이디 */}
             <input type="text" placeholder="아이디" className={`${input_element}`}></input>
             <div className={"text-left w-60 text-red-500"}>아이디가 중복되었습니다.</div>
+            {/* 비밀번호 */}
             <input type="password" placeholder="비밀번호" className={`${input_element}`}></input>
             <input type="password" placeholder="비밀번호 재확인" className={`${input_element}`}></input>
             <div className={"text-left w-60 text-green-500"}>비밀번호가 일치합니다.</div>
@@ -105,6 +113,24 @@ export default function Login() {
               <span className={`whitespace-nowrap ${text_opacity}`}>회원정보</span>
               <div className="flex-1 h-px bg-white"></div>
             </div>
+            {/* 회원정보 */}
+            {/* 이름 */}
+            <input type="text" placeholder="이름" className={`${input_element}`}></input>
+            {/* 성별 */}
+            <div className={`flex w-60 h-8 rounded-sm`}>
+              <button onClick={(e)=>{handleChangeSexButton(e,"male")}} className={`${sexButton} ${sexButton === "male" ? "bg-gray-400" : "bg-white"} block w-32 text-black border-black border-r`}>남자</button>
+              <button onClick={(e)=>{handleChangeSexButton(e,"female")}} className={`${sexButton} ${sexButton === "female" ? "bg-gray-400" : "bg-white"} block w-32 text-black`}>여자</button>
+            </div>
+            {/* 주민번호 */}
+            <div className="flex justify-between w-60">
+              <input type="text" placeholder="주민번호 앞자리" className={`w-28 h-8 font-sans text-black ${input_element}`}></input>
+              <div className="flex items-center">-</div>
+              <input type="text" placeholder="뒷 자리" className={`w-28 h-8 font-sans text-black ${input_element}`}></input>
+            </div>
+            {/* 이메일 */}
+            <input type="text" placeholder="e-mail" className={`${input_element} placeholder-font-sans`}></input>
+
+            {/* 로그인으로 돌아가기 */}
             <div onClick={addSignin} className={`h-[20px] cursor-pointer ${text_size} ${text_opacity} ${hover} mt-32`}>계정이 있으신가요 ? <span className={"text-white text-opacity-100"}>로그인</span></div>
           </form>
         </div>
