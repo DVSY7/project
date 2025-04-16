12261ee3f3492770f901d3309550e1e0fb8e2b45
DVSY7 <gbytkdduf1004@naver.com>
2025-04-16 18:45:42 +0900

    feat:커뮤니티 레이아웃 설정정

--- client/src/componant/community.js
+++ client/src/componant/community.js
@@ -2,6 +2,12 @@ import '../App.css';
 import Menu from './menu';
 
 export default function Community() {
+  // 플렉스 요소 가로세로 센터
+  const flexCenter = "flex justify-center items-center";
+  // 플렉스 요소 세로 센터
+  const flexColCenter = "flex justify-start items-center";
+  // 레이아웃 그림자
+  const shadow = "shadow-xl border border-solid border-t-gray-200";
   
   return (
     <>
@@ -10,7 +16,14 @@ export default function Community() {
         {/* 왼쪽: 가로 1 비율 (1/9) */}
         <Menu current_src ={2}/>
         {/* 오른쪽: 가로 8 비율 (8/9) */}
-        <div className=" flex flex-col flex-wrap bg-green-200 row-span-9 sm:col-span-8 "></div>
+        <div className=" flex flex-nowrap row-span-9 sm:col-span-8 ">
+          <div className={` ${flexCenter} w-[30%] min-w-[350px] h-full`}>
+            <div className={`${shadow} rounded-3xl w-[87%] h-[93%]`}></div>
+          </div>
+          <div className={`${flexColCenter} w-[70%] h-full `}>
+            <div className={`${shadow} rounded-3xl w-[97%] h-[93%]`}></div>
+          </div>
+        </div>
       </div>
     </>
   );
