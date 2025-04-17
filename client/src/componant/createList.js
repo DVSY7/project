3beb0f5073c80009086afc707637dba4b16bd403
kimrachelponchan <jgim9182@gmail.com>
2025-04-17 21:52:38 +0900

    feat:만들기 리스트 자유형 작성폼 구현

--- client/src/componant/createList.js
+++ client/src/componant/createList.js
@@ -4,6 +4,8 @@ import Menu from "./menu.js";
 export default function CreateList() {
   const [isPlanned, setIsPlanned] = useState(true); // 계획형인지 여부
   const [Group, setGroup] = useState(true); 
+  const [Offline, setOffline] = useState(true); // 대면인지 여부
+  
   return (
     <>
       {/*가장 바깥영역 화면의 최대로 설정*/}
@@ -14,14 +16,14 @@ export default function CreateList() {
         <div className=" flex flex-col flex-wrap bg-green-200 row-span-9 sm:col-span-8 ">
           <div className="flex items-center justify-center w-full h-full">
             <div
-              className={`w-[95%] h-[93%] rounded-3xl flex flex-row bg-white`}
+              className={`w-[95%] h-[93%] rounded-3xl grid grid-cols-[3fr_2fr]  bg-white`}
             >
               {/* 왼쪽쪽 영역 */}
-              <div className="flex-grow-2 grid grid-rows-[1fr_6.5fr_0.5fr] p-4 pl-10">
+              <div className="grid grid-rows-[1fr_6.5fr_0.5fr] p-4 pl-10">
                 {/* 상단 영역 */}
                 <div className="flex grid grid-rows-[0.5fr_1fr]">
                   <div className="flex-1 flex items-center justify-between">
-                    <div className="text-xl">20xx.MM.DD</div>
+                    <div className="sm:text-xl text-lg">20xx.MM.DD</div>
                     <div>
                       <button className="mr-2 border px-4 py-0.5 rounded" onClick={() => setIsPlanned(!isPlanned)}>
                         {isPlanned ? "계획형" : "자유형"}
@@ -30,15 +32,16 @@ export default function CreateList() {
                         {Group ? "같이하기" : "혼자하기"}
                       </button>
                       {/*Group이 true일 때만 인원 셀렉트 박스 렌더링*/}
-                      {Group && (}
+                      {Group && (
                       <select className="mr-2 h-[30px] w-16 rounded border">
                         <option value="2">2</option>
                         <option value="3">3</option>
                         <option value="4">4</option>
                         <option value="5">5</option>
                       </select>
-                      <button className="border px-4 py-0.5 rounded">
-                        대면
+                      )}
+                      <button className="border px-4 py-0.5 rounded" onClick={() => setOffline(!Offline)}>
+                        {Offline ? "대면" : "비대면"}
                       </button>
                     </div>
                   </div>
@@ -97,8 +100,22 @@ export default function CreateList() {
                 </div>
               </div>
               {/* 오른쪽 영역 */}
-              <div className="flex-grow-2 bg-blue-100 flex items-center justify-center">
-                2
+              <div className="border-l grid grid-rows-[2.5fr_3fr_0.5fr]  overflow-hidden">
+                {/* 상단 영역: 지도영역 */}
+                <div className="bg-red-200">
+                  지도
+                </div>
+                {/* 중간 영역: 텍스트 입력란 */}
+                <div className=" flex items-center justify-center">
+                  <textarea className="w-[90%] h-[90%] border rounded p-2 resize-none"
+                  placeholder="소개 글을 자유롭게 입력하세요">
+                    
+                  </textarea>
+                </div>
+                {/* 끝 영역: 텍스트 입력란 */}
+                <div className="flex justify-end items-center">
+                      <button className="bg-blue-500 text-white h-10 mr-8 px-3 rounded-xl">등록하기</button>
+                </div>
               </div>
             </div>
           </div>
