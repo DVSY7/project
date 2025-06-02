26d6a97d1cade1e1e6f3094cbbf96723ae7145a3
kimrachelponchan <jgim9182@gmail.com>
2025-06-02 09:38:26 +0900

    fix: 다중선택-> 하나만 선택 문구 변경 + 필요없는 css 정리

--- client/src/componant/ai/WhoWithStep.js
+++ client/src/componant/ai/WhoWithStep.js
@@ -9,14 +9,14 @@ export default function WhoWithStep({onNext, onBack}){
     return(
     <div className="bg-white rounded-lg w-[45%] h-[80%] max-w-full relative flex items-center justify-center bg-blue-400">
         <div className="absolute flex flex-col items-center w-[87%] h-full py-16 justify-between">
-            <div className="basis-1/5 w-full flex flex-col items-center">
+            <div className="basis-2/6 w-full flex flex-col items-center">
             <button onClick={onBack} className='flex text-2xl py-4 self-start '>←</button>
                 <div className="flex text-6xl">😎</div>
 
                 <div className="flex-1 flex text-3xl items-end">누구와 떠나나요?</div>
-                <div className="flex-1 flex justify-center items-start">다중 선택이 가능해요</div>
+                <div className="flex-1 flex justify-center items-end">이번 여행의 동반자를 선택해주세요</div>
             </div>
-            <div className="basis-3/5 w-full flex justify-center mt-8">
+            <div className="basis-3/6 w-full flex justify-center mt-8">
                 <div className="w-[75%] h-full">
                     <div className="grid grid-cols-3 gap-2">
                         {options.map(opt => (
@@ -25,7 +25,7 @@ export default function WhoWithStep({onNext, onBack}){
                     </div>
                 </div>
             </div>
-            <div className="basis-1/5 w-full flex flex-col justify-end">
+            <div className="basis-1/6 w-full flex flex-col justify-end">
                 <button
                 onClick={onNext}
                 disabled={!selected}
