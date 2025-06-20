2d1c47f74551926cb159e7ec63c67d8795e79a92
kimrachelponchan <jgim9182@gmail.com>
2025-06-20 21:44:17 +0900

    feat: 게시글 작성 폼 구현중

--- client/src/componant/createPost.js
+++ client/src/componant/createPost.js
@@ -2,13 +2,89 @@ import Menu from "./menu"
 
 export default function createPost(){
     return(
+        // <>
+        // <div className="grid grid-cols-1 grid-rows-9 sm:grid-rows-1 sm:grid-cols-sm-minmax sm:grid-cols-9 h-screen overflow-x-hidden">
+        // {/* 왼쪽: 가로 1 비율 (1/9) */}
+        // <Menu current_src={5} />
+        // {/* 오른쪽: 가로 8 비율 (8/9) */}
+        // <div className="flex flex-col flex-wrap row-span-9 sm:col-span-8 ">
+        //     </div></div>
+        // </>
         <>
-        <div className="grid grid-cols-1 grid-rows-9 sm:grid-rows-1 sm:grid-cols-sm-minmax sm:grid-cols-9 h-screen overflow-x-hidden">
-        {/* 왼쪽: 가로 1 비율 (1/9) */}
-        <Menu current_src={5} />
-        {/* 오른쪽: 가로 8 비율 (8/9) */}
-        <div className="flex flex-col flex-wrap row-span-9 sm:col-span-8 ">
-            </div></div>
+            <div
+                className={`flex justify-center items-center fixed bg-black bg-opacity-50 w-screen h-screen left-0 top-0 z-20`}
+            >
+                <div className={`flex relative w-[60vw] h-[75vh] rounded-md bg-white bg-opacity-100`}>
+                   
+                    <span
+                        // 갤러리 모달 닫기 버튼
+                        // onClick={() => setClickedGallery(null)}
+                        className={`flex justify-end absolute right-0 mr-2 text-[1.5rem] h-[60px] w-[50px] cursor-pointer text-gray-500 hover:text-black transition-colors duration-500`}>
+                        {/* 닫기버튼 요소 */}
+                        X
+                    </span>
+                    {/* 갤러리 이미지 영역 */}
+                    <div className={`relative w-[60%] rounded-l-md`}>
+                        {/* 갤러리 이미지 */}
+                        <div className="bg-blue-100 w-full h-full">
+                            이미지를 선택하세요
+                        </div>
+                        {/* <img
+                        // 이미지를 눌러도 다음이미지로 넘어가도록 설정 
+                       
+                        className={`opacity-100 w-full h-full rounded-l-md cursor-pointer transition-opacity duration-500`}></img> */}
+                        {/* 갤러리 이미지 컨트롤러 */}
+                        {/* <div className={`absolute bottom-4 w-full h-[50px] flex justify-center items-center `}>
+                      
+                        </div> */}
+                    </div>
+                    {/* 갤러리 본문 영역 */}
+                    <div className={`w-[40%] rounded-r-md`}>
+                        {/* 갤러리 제목 요소 */}
+                        <div className={`h-[10%] flex items-center pl-4 border-b-[2px] border-gray-200`}>
+                            {/* 갤러리 제목 프로필 */}
+                            <img className={`w-[40px] h-[40px] mr-4`} src="images/미니프로필.png" alt="미니프로필"></img>
+                            {/* <span>{username}의 게시글</span> */}
+                            <span>레이첼</span>
+                        </div>
+                        {/* 갤러리 본문 요소 */}
+                        <div className={`h-[32.5%] flex border-b-[2px] border-gray-200`}>
+                            {/* 갤러리 본문 프로필 */}
+                            <div className={`w-[15%] flex justify-center mt-4`}>
+                                {/* 갤러리 본문 프로필 요소 */}
+                                <img className={`w-[40px] h-[40px]`} src={`images/미니프로필.png`} alt="미니프로필"></img>
+                            </div>
+                            {/* 갤러리 본문 내용 및 태그 영역 */}
+                            <div className={`w-[85%]`}>
+                                {/* 갤러리 본문 내용 */}
+                                <div className={`h-[87%] flex justify-center items-center bg-blue-300`}>
+                                    <textarea className="w-full h-[80%]" placeholder="글을 입력하세요"></textarea>
+                                </div>
+                                {/* 갤러리 본문 태그 */}
+                                <div className={`h-[13%]`}>
+                                    #<input type="text" placeholder="태그입력"></input>
+                                </div>
+                            </div>
+
+                        </div>
+                        {/* 갤러리 댓글 영역 */}
+                        <div className={`h-[37%] overflow-y-auto hide-scrollbar`}>
+                           
+                        </div>
+                        {/* 갤러리 아이콘 영역 */}
+                        <div className={`h-[13%] pl-4 font-sans font-bold text-[0.8rem] border-b-[2px] border-gray-200`}>
+                        
+                        </div>
+                        {/* 갤러리 댓글 입력창 */}
+                        <div className={`h-[7.5%] flex`}>
+                            {/* 갤러리 댓글 인풋 영역 */}
+                            <div className={`resize-none outline-none w-[85%] h-full pt-4 pl-4 `}></div>
+                            {/* 갤러리 댓글 게시버튼 */}
+                            <div className={`w-[15%] flex justify-center items-center text-blue-500`}>공유하기</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
