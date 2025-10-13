830b5bf6e32256812dc38ac6bd0af433b5ca4b6c
DVSY7 <gbytkdduf1004@naver.com>
2025-10-14 01:52:00 +0900

    fix: 섬네일 확인후 롤백

--- client/src/componant/content/footer.js
+++ client/src/componant/content/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <>
             <div className={"hidden sm:flex h-[30%] w-full sm:h-[10%] justify-evenly items-center flex-nowrap font-sans text-[15px] text-gray-400"}>
                 <div className="">COPYRIGHT © by WEBDESIGN. ALL RIGHTS RESERVED</div>
-                <div className="">모든 아이콘의 출처 : instagram</div>
+                <div className="">모든 아이콘의 출처 : FLATICON</div>
                 <div className="">모든 아이콘 url : https://www.flaticon.com/kr/icons</div>
             </div>
 
