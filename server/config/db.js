27a57d32171285f40ae6f0ddc008167aa6fb2144
DVSY7 <gbytkdduf1004@naver.com>
2025-10-13 14:20:32 +0900

    fix: db연결 배포적용

--- server/config/db.js
+++ server/config/db.js
@@ -4,10 +4,10 @@ require('dotenv').config({ path: './.env' });
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '0000',
-    database: 'bucketmate',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '0000',
+    database: process.env.DB_NAME || 'bucketmate',
     port: process.env.DB_PORT || 3400 // 기본값 3400 설정
 });
 
