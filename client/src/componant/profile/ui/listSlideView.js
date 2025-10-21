fc76a066b0732042f69e1ac4c5cae5db64238562
DVSY7 <gbytkdduf1004@naver.com>
2025-10-22 00:55:48 +0900

    teat: 리스트 슬라이드 UI개발 3

--- client/src/componant/profile/ui/listSlideView.js
+++ client/src/componant/profile/ui/listSlideView.js
@@ -1,12 +1,15 @@
 //client/src/componant/profile/ui/listSlideView.js
 import {useState,useEffect,useRef} from "react";
 import { fetchListSlides } from "../api/ListSlide";
+import { changeInterestColor } from "../utilities/interestColor";
 
 
 export const ListSlideView = ({ listID,listContainRef,onClose}) =>{
     const [slideAnimate,setSlideAnimate] = useState("slide-in-right");
     const slideRef = useRef();
     const [listClicked, setListClicked] = useState(false);
+    const [interestBackGround, setInterestBackGround] = useState("");
+    const [endDate,setEndDate] = useState();
     // list loading 상태 애니메이션
     const listLoadingAnimation = "p-2 inline-block rounded-lg shimmer-loader"
 
@@ -19,7 +22,7 @@ export const ListSlideView = ({ listID,listContainRef,onClose}) =>{
                 const listDetailsData = await fetchListSlides(listID);
                 setListClicked(false);
                 setListDetails(null);
-                await new Promise((resolve) => setTimeout(resolve, 1000));
+                await new Promise((resolve) => setTimeout(resolve, 300));
                 if(listDetailsData){
                     setListClicked(true);
                     setListDetails(listDetailsData);
@@ -56,14 +59,70 @@ export const ListSlideView = ({ listID,listContainRef,onClose}) =>{
         };
     }, [onClose])
 
+    // 관심사 텍스트 색상 설정
+    useEffect(()=>{
+        if(listDetails?.listBasic?.interest){
+            const interestText = listDetails.listBasic.interest;
+            const interestResult = interestText.slice(0, interestText.indexOf("&")).trim();
+
+            const interestColor = changeInterestColor(interestResult)
+            setInterestBackGround(interestColor);
+        }
+    },[listDetails])
+
+    useEffect(()=>{
+        if(listDetails?.listBasic?.end_date){
+            const endDateText = listDetails.listBasic.end_date;
+            const endDateResult = endDateText.slice(0,endDateText.indexOf("T")).trim();
+
+            setEndDate(endDateResult);
+        }
+    })
+
     return(
         <>
             <div 
                 ref={slideRef}
-                className={`${slideAnimate} border-[2px] rounded-3xl rounded-r-none shadow-lg border-gray-100 bg-white w-[58vw] h-[75vh] absolute p-4 right-0 z-20`}
+                className={`${slideAnimate} border-[2px] rounded-3xl rounded-r-none shadow-lg border-gray-100 bg-white w-[58vw] h-[75vh] absolute p-4 right-0 overflow-y-scroll z-20`}
             >
-                <div className={`${listClicked ? "" : listLoadingAnimation } h-20 w-full`}>{listDetails?.listBasic?.title || ""}</div>
-                <div className="bg-gray-200 h-[45vh]">일차</div>
+                <div className={`${listClicked ? "border-b-[3px] border-gray-100" :listLoadingAnimation} mb-2 flex h-20 w-full`}>
+                    <div className={"h-full w-[40%] text-[1.5em] flex items-center"}>{listDetails?.listBasic?.title}</div>
+                    <div className={`flex items-center w-[20%]`}>
+                        {listDetails&&(
+                            <div className={`px-2 py-1 ${interestBackGround} rounded-full`}>{listDetails?.listBasic?.interest}</div>
+                        )}
+                    </div>
+                    <div className={"h-full w-[30%] flex items-center justify-center gap-4"}>
+                        {listDetails&& 
+                            <>
+                                <div>{listDetails?.listBasic?.is_group? "같이하기" : "혼자하기"}</div>
+                                <div>{listDetails?.listBasic?.is_offline? "대면" : "비대면"}</div>
+                                <div>{listDetails?.listBasic?.is_planned? "계획형" : "비계획형"}</div>
+                            </>
+                        }
+                    </div>
+                    {listDetails&&
+                        <div className="flex items-center w-[10%]">최대인원 : {listDetails?.listBasic?.max_participants}</div>
+                    }
+                </div>
+                <div className="flex justify-between h-[45vh]">
+                    <div className={`${listClicked? "" : listLoadingAnimation} h-full w-[55%] rounded-lg`}></div>
+                    <div className={`${listClicked? "" : listLoadingAnimation} h-full w-[43%] rounded-lg`}></div>
+                </div>
+                <div className={`h-[15vh] mt-2 flex gap-4`}>
+                    {/* 📅📍💰⏳ */}
+                    <div className={`${listClicked? "bg-red-100" : listLoadingAnimation} h-full w-[25%] rounded-lg flex flex-col justify-center items-center`}>
+                        {listDetails&&(
+                            <>
+                                <div className={`text-[1.3em] my-2 font-bold w-full ml-4`}>📅 마감 일자 </div>
+                                <div className={`bg-white h-[30%] w-[90%] rounded-md flex justify-center items-center font-bold`}>{listDetails?.listBasic?.end_date === null ? "없음" : `${endDate}`}</div>
+                            </>
+                        )}
+                    </div>
+                    <div className={`${listClicked? "bg-yellow-100" : listLoadingAnimation} h-full w-[25%] rounded-lg`}></div>
+                    <div className={`${listClicked? "bg-green-100" : listLoadingAnimation} h-full w-[25%] rounded-lg`}></div>
+                    <div className={`${listClicked? "bg-blue-100" : listLoadingAnimation} h-full w-[25%] rounded-lg`}></div>
+                </div>
             </div>
         </>
     )
