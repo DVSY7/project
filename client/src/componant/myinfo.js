2d5d819b1696e9be4cc472a4a68a6eb487e3ba20
DVSY7 <gbytkdduf1004@naver.com>
2025-11-04 21:28:28 +0900

    feat: 내 정보 UI 디자인 수정

--- client/src/componant/myinfo.js
+++ client/src/componant/myinfo.js
@@ -8,7 +8,7 @@ export default function MyInfo(){
                 {/* 왼쪽: 가로 1 비율 (1/9) */}
                 <Menu/>
                 {/* 오른쪽: 가로 8 비율 (8/9) */}
-                <div className=" flex flex-col flex-wrap justify-center items-center bg-gradient-to-l from-blue-300 to-blue-50 row-span-9 sm:col-span-8 ">
+                <div className=" flex flex-col flex-wrap justify-center items-center bg-gray-50 row-span-9 sm:col-span-8 ">
                     <InfoUI/>
                 </div>
             </div>
@@ -22,10 +22,10 @@ export const InfoUI = () => {
             <div
                 className={`flex gap-6 w-[60%] h-[80%]`}
             >
-                <div className={`caret-transparent relative flex flex-col justify-center items-center border-[2px] border-gray-400 bg-white w-[25%]`}>
+                <div className={`border-[2px] border-gray-200 caret-transparent relative flex flex-col justify-center items-center bg-white shadow-xl w-[25%] rounded-md`}>
                     <InfoItem1/>
                 </div>    
-                <div className={`flex flex-col border-[2px] border-gray-400 bg-white w-[75%]`}>
+                <div className={`flex flex-col bg-white border-[2px] border-gray-200 w-[75%] shadow-xl rounded-lg`}>
                     <InfoItem2/>
                 </div>
             </div>
@@ -38,7 +38,7 @@ export const InfoItem1 = () => {
         <>
             {/* 프로필 이미지 */}
             <img 
-                src={`images/미니프로필.png`}
+                src={`images/profile/profile (4).jpg`}
                 className={`w-[80%] aspect-square mb-4 rounded-full`}
                 >
             </img>
@@ -48,7 +48,7 @@ export const InfoItem1 = () => {
             <div className={`bg-gray-300 px-2 rounded-full my-2`}>basic</div>
             {/* 회원가입일 */}
             <div className={`text-[1.5vh] mt-2 mb-4 font-sans text-gray-500`}>가입일: 2023-01-01</div>
-            <button className={`text-[1.2vh] absolute bottom-6 border-[0.2vh] border-gray-500 rounded-full px-2 py-1 text-red-600`}>계정삭제</button>
+            <button className={`text-[1.2vh] absolute bottom-16 px-2 py-1 text-red-600`}>계정삭제</button>
         </>
     )
 }
@@ -56,38 +56,43 @@ export const InfoItem1 = () => {
 export const InfoItem2 = () => {
     return (
         <>
-            <div className={`h-[7%] font-sans text-[1.5vh]`}>
-                <button className={`bg-blue-500 border-b-[2px] border-r-[2px] border-gray-400 text-white py-2 px-4 `}>정보수정</button>
-                <button className={`bg-white border-r-[2px] border-gray-400 border-b-[2px] text-gray-600 py-2 px-4 `}>활동 리포트</button>
+            <div className={`h-[10%] font-sans text-[1.5vh] flex flex-col justify-center items-center mt-10`}>
+                <div className={`text-[3.5vh] font-lilita mb-4`}>User Information</div>
+                <div>
+                    <button className={`border-b-[3px] border-blue-500 text-gray-600 py-2 px-4 rounded-tl-md font-extrabold`}>정보수정</button>
+                    <button className={`border-b-[3px] border-gray-300 text-gray-600 py-2 px-4 font-extrabold`}>활동 리포트</button>
+                </div>
             </div>
-            <div className={`h-[93%] flex flex-col justify-center items-center gap-4`}>
+            
+            <div className={`h-[93%] flex flex-col justify-center items-center gap-10`}>
+                
                 {/* 닉네임 */}
                 <div className={`w-[45%] flex justify-end`}>
-                    <label>NickName:</label>
-                    <input type="text" className="ml-4 border-[2px] border-black pl-2" value={`뺀찌TV`}></input>
+                    <label className={`text-[2vh]`}>NickName:</label>
+                    <input type="text" className="ml-4 border-b-[1px] border-black pl-2" value={`뺀찌TV`}></input>
                 </div>
                 {/* 생년월일 */}
                 <div className={`w-[45%] flex justify-end`}>
-                    <label>BirthDate:</label>
-                    <input type="text" className={`ml-4 border-[2px] border-black pl-2`} value={`1998-07-24`}></input>
+                    <label className={`text-[2vh]`}>BirthDate:</label>
+                    <input type="text" className={`ml-4 border-b-[1px] border-black pl-2`} value={`1998-07-24`}></input>
                 </div>
                 {/* 성별 */}
                 <div className={`w-[45%] flex justify-end`}>
-                    <label>Gender:</label>
-                    <input type={`text`} className={`ml-4 border-[2px] border-black pl-2`} value={`남자`}></input>
+                    <label className={`text-[2vh]`}>Gender:</label>
+                    <input type={`text`} className={`ml-4 border-b-[1px] border-black pl-2`} value={`남자`}></input>
                 </div>
                 {/* 거주지역 */}
                 <div className={`w-[45%] flex justify-end`}>
-                    <label>Location:</label>
-                    <input type={`text`} className={`ml-4 border-[2px] border-black pl-2`} value={`광주 광역시`}></input>
+                    <label className={`text-[2vh]`}>Location:</label>
+                    <input type={`text`} className={`ml-4 border-b-[1px] border-black pl-2`} value={`광주 광역시`}></input>
                 </div>
                 {/* 관심사 */}
                 <div className={`w-[45%] flex justify-end`}>
-                    <label>Interests:</label>
-                    <input type={`text`} className={`ml-4 border-[2px] border-black pl-2`} value={`없음`}></input>
+                    <label className={`text-[2vh]`}>Interests:</label>
+                    <input type={`text`} className={`ml-4 border-b-[1px] border-black pl-2`} value={`없음`}></input>
                 </div>
                 {/* 수정하기 버튼 */}
-                <div className={`mt-20 bg-blue-500 text-white p-2 px-4 rounded-full`}>수정하기</div>
+                <div className={`caret-transparent mt-20 bg-blue-500 text-white p-2 px-4 rounded-full cursor-pointer`}>수정하기</div>
             </div>
         </>
     )
